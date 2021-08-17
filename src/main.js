1291c1a2895f12ec0a1e07af6c60a6789a2e521a
Richard Snider <richardnsnider@gmail.com>
2021-08-17 04:18:21 +0000

    add elemancer content

--- src/main.js
+++ src/main.js
@@ -1,9 +1,33 @@
+const loop = require('@vorprog/loop').loop;
+
 /**
- * @typedef {Object} Module
- * @property {function(number,number):number|function(string,string):string} add
+ * @typedef {Object} ElementConfig
+ * @property {string} tag - HTML element tag
+ * @property {string} xmlns - XML namespace (defaults to http://www.w3.org/1999/xhtml)
+ * @property {string} textContent
+ * @property {ElementConfig[]} children
+ * @property {EventListener} onclick
  */
 
-/** @type {Module} */
-module.exports = {
-  add: (arg0, arg1) => arg0 + arg1
-}
+/**
+ * @param {HTMLElement} parentElement
+ * @param {ElementConfig} elementConfig
+ * @returns {Element}
+*/
+const newElement = (parentElement, elementConfig = {}) => {
+  const element = document.createElementNS(elementConfig.xmlns || `http://www.w3.org/1999/xhtml`, elementConfig.tag || `div`);
+  element.textContent = elementConfig.textContent || null;
+  loop(elementConfig.children, (key, childConfig) => newElement(element, childConfig));
+  if(elementConfig.onclick) element.addEventListener("click", elementConfig.onclick);
+
+  delete elementConfig.xmlns;
+  delete elementConfig.tag;
+  delete elementConfig.textContent;
+  delete elementConfig.children;
+  delete elementConfig.onclick;
+
+  loop(elementConfig, (key, value) => element.setAttribute(key, value || ``));
+  return parentElement.appendChild(element);
+};
+
+module.exports = newElement;
--- tests/test.js
+++ tests/test.js
@@ -1,17 +1,19 @@
 const diff = require('diff');
+const jsdom = require('jsdom');
 const loop = require('@vorprog/loop').loop;
 const main = require('../src/main');
 
-const testResults1 = diff.diffWords(`33`, main.add(11, 22).toString());
-if (testResults1.length > 1) {
-  const diffLog = loop(testResults1, (index, change) => `${change.added ? `DISCREPANCY.ACTUAL:` : change.removed ? `DISCREPANCY.EXPECTED:` : ``} ${change.value}`).join(`\n`);
-  throw Error(`There were unexpected results during testing: \n${diffLog}`);
+const checkForDiscrepancies = (expected, actual) => {
+  const differences = diff.diffJson(expected, actual);
+  if (differences.length > 1) {
+    const diffLog = loop(differences, (index, difference) => `${difference.added ? `DISCREPANCY.ACTUAL:` : difference.removed ? `DISCREPANCY.EXPECTED:` : ``} ${difference.value}`).join(``);
+    throw Error(`Found unexpected discrepancies: \n${diffLog}`);
+  }
 }
 
-const testResults2 = diff.diffWords(`hello world!`, main.add(`hello `, `world!`));
-if (testResults2.length > 1) {
-  const diffLog = loop(testResults2, (index, change) => `${change.added ? `DISCREPANCY.ACTUAL:` : change.removed ? `DISCREPANCY.EXPECTED:` : ``} ${change.value}`).join(`\n`);
-  throw Error(`There were unexpected results during testing: \n${diffLog}`);
-}
+const dom = new jsdom.JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
+document = dom.window.document //TODO: figure out errors with jsdom
+// const newElement = main(dom.window.document, { tag: `div`});
+// checkForDiscrepancies(`div`, newElement.tagName);
 
 console.log(`Tests successfully completed!`);
